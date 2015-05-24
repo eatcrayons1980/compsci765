225093651d47691264338adfe253163d49312e33
Paul Freeman <pfre484@aucklanduni.ac.nz>
2015-05-24 15:39:39 +1200

    basic questions about previous trips implemented

--- source/driver.py
+++ source/driver.py
@@ -5,34 +5,50 @@ from pyke import krb_traceback
 import sys
 import datetime
 
-engine = knowledge_engine.engine(__file__)
+def run():
+    init_shortterm_memory()
+    init_user()
 
+def init_shortterm_memory():
+    shortterm = open('shortterm.kfb', 'w')
+    shortterm.write("# Activated " + str(datetime.datetime.now()) + "\n")
+    shortterm.close()
 
-def test():
+def init_user():
+    engine = knowledge_engine.engine(__file__)
     engine.reset()
-
     engine.activate('travelrules')
-    #engine.trace('travelrules', 'new_user')
-
-    #f = open('travelfacts.kfb', 'w')
-    #s = sys.stdout
-
-    #sys.stdout = f
-    #engine.get_kb('travelfacts').dump_universal_facts()
-    #engine.get_kb('travelfacts').dump_specific_facts()
-    #sys.stdout = s
 
     try:
-        res, plan = engine.prove_1_goal('travelrules.new_user($user)')
+        result, plan = engine.prove_1_goal('travelrules.new_user($user)')
     except knowledge_engine.CanNotProve:
         try:
-            res, plan = engine.prove_1_goal('travelrules.existing_user($user)')
+            result, plan = engine.prove_1_goal('travelrules.existing_user($user)')
         except knowledge_engine.CanNotProve:
+            print("Could not establish user model. Exiting with -1.")
             sys.exit(-1)
-        else:
-            print("Welcome back!")
-    else:
-        print("Welcome to the travel buddy!")
-        
 
+    write_shortterm_longterm(engine)
+
+def write_shortterm(engine):
+    shortterm = open('shortterm.kfb', 'a')
+    temp = sys.stdout
+    sys.stdout = shortterm
+    engine.get_kb('shortterm').dump_universal_facts()
+    engine.get_kb('shortterm').dump_specific_facts()
+    sys.stdout = temp
+    shortterm.close()
+
+def write_longterm(engine):
+    longterm = open('travelfacts.kfb', 'a')
+    temp = sys.stdout
+    sys.stdout = longterm
+    print("\n\n# Added " + str(datetime.datetime.now()))
+    engine.get_kb('travelfacts').dump_specific_facts()
+    sys.stdout = temp
+    longterm.close()
+
+def write_shortterm_longterm(engine):
+    write_shortterm(engine)
+    write_longterm(engine)
     
